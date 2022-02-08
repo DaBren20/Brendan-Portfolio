0b9eb738eab9b68013db6214571fbba1a71d3990
DaBren20 <dabrentwenty@gmail.com>
2022-02-08 00:26:32 -0500

    Removed personal info

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,7 +6,6 @@ const Info = () => {
         <h1>Contact Me!</h1>
         <h2>If you are interested in my services please contact me!</h2><br />
         <p><b>Email:</b> Brendan.prosper@gmail.com</p>
-        <p><b>Phone #: </b>647-706-7637</p>
         <hr />
         <h4>Other Links</h4>
         <a className="btn btn-outline-dark btn-floating m-1" href="https://www.linkedin.com/in/brendan-polius-prosper-b4bb59225/" role="button"><i className="bi bi-linkedin"></i></a>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,14 +10,13 @@ const Menu = () => {
                 <Link className="navbar-brand" to="/">Home</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
-                    
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                    <Link className="nav-item nav-link" to="/about">About Me</Link>
-                    <Link className="nav-item nav-link" to="/projects">Projects</Link>
-                    <Link className="nav-item nav-link" to="/contact">Contact Me</Link>
-                </div>
+                        <Link className="nav-item nav-link" to="/about">About Me</Link>
+                        <Link className="nav-item nav-link" to="/projects">Projects</Link>
+                        <Link className="nav-item nav-link" to="/contact">Contact Me</Link>
+                    </div>
                 </div>
             </nav>
         </div>
