9d4ccdbd316ac3ad419d9f0ac125662b6997579a
DaBren20 <dabrentwenty@gmail.com>
2022-02-07 21:51:03 -0500

    Fixed pdf

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,6 +10,7 @@ const Menu = () => {
                 <Link className="navbar-brand" to="/">Home</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
+                    
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
